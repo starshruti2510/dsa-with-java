6018eb2c800d9a86d0f372539e050f231c460cd4
Student Shruti <shrutisharma87860@gmail.com>
2026-06-23 17:57:04 +0530

    Transpose of matrix

---
+++ src/Arrays/TransposeMatrix.java
@@ -0,0 +1,33 @@
+package src.Arrays;
+
+import java.util.Scanner;
+
+public class TransposeMatrix {
+    public static void transpose(int[][] arr) {
+        
+        //LOWER TRIANGLE OF A MATRIX
+        for(int i=0; i < arr.length; i++) {
+            for(int j=0; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        //TRANSPOSE OF MATRIX
+        for(int i=0; i < arr.length; i++) {
+            for(int j=0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3,4},
+                        {5,6,7,8},
+                    {9,10,11,12},
+                {13,14,15,16}};
+        transpose(arr);
+
+    }
+}
