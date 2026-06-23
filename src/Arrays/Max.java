695a68f4e4bb5dabc1e9188cc72e1000d1b4a796
Student Shruti <shrutisharma87860@gmail.com>
2026-06-23 15:57:53 +0530

    Maximum of an array

---
+++ src/Arrays/Max.java
@@ -0,0 +1,28 @@
+package src.Arrays;
+
+import java.util.*;
+
+public class Max {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of an array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the numbers of array: ");
+        for(int i=0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //MAXIMUM OF AN ARRAY
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
