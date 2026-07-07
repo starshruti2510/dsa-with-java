eda3f4bb795ac7b735b495321448dd500d39e9d0
Student Shruti <shrutisharma87860@gmail.com>
2026-07-07 15:30:15 +0530

    Completed the Transpose Matrix

--- src/Arrays/TransposeMatrix.java
+++ src/Arrays/TransposeMatrix.java
@@ -16,7 +16,7 @@ public class TransposeMatrix {
 
         //TRANSPOSE OF MATRIX
         for(int i=0; i < arr.length; i++) {
-            for(int j=0; j < arr[0].length; j++) {
+            for(int j=0; j < arr[0].length; j++) {          // ---> Printing the transpose
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
@@ -25,7 +25,7 @@ public class TransposeMatrix {
     public static void main(String[] args) {
         int[][] arr = {{1,2,3,4},
                         {5,6,7,8},
-                    {9,10,11,12},
+                    {9,10,11,12},                       // ----> Calling the Transpose Matrix method
                 {13,14,15,16}};
         transpose(arr);
 
