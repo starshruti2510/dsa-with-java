d47e98c38e3ae6ef80b6df9bacd4e4ce04127703
Student Shruti <shrutisharma87860@gmail.com>
2026-07-11 23:21:52 +0530

    Corrected typo in file name

--- src/Arrays/Max.java
+++ src/Arrays/Max.java
@@ -16,7 +16,7 @@ public class Max {
         }
 
         //MAXIMUM OF AN ARRAY
-        int max = Integer.MIN_VALUE;
+        int max = arr[0];
         for(int i=0; i < arr.length; i++) {
             if(arr[i] > max) {
                 max = arr[i];
