79c0a1d390147717247890f1fd7f229f75ad3795
Student Shruti <shrutisharma87860@gmail.com>
2026-08-06 23:52:12 +0530

    Selection Sort for Largest element

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -9,11 +9,12 @@ public class SelectionSort {
     }
 
     public static void selectionSort(int[] nums) {
+        // Find Smallest Element first
         int n = nums.length;
         for(int i=0; i<n-1; i++) {
-            int min = Integer.MAX_VALUE, minIndex = -1;
+            int min = nums[i], minIndex = i;
 
-            for(int j=i; j<n; j++) {
+            for(int j=i+1; j<n; j++) {
                 if(nums[j] < min) {
                     min = nums[j];
                     minIndex = j;
@@ -23,6 +24,23 @@ public class SelectionSort {
             int temp = nums[i];
             nums[i] = nums[minIndex];
             nums[minIndex] = temp;
+        }
+
+        Find Largest Element first
+        int n = nums.length;
+        for(int i=n-1; i>0; i--) {
+            int max = Integer.MIN_VALUE, maxIndex = 0;
+
+            for(int j=0; j<=i; j++) {
+                if(nums[j] > max) {
+                    max = nums[j];
+                    maxIndex = j;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[maxIndex];
+            nums[maxIndex] = temp;
         }
     }
     public static void main(String[] args) {
