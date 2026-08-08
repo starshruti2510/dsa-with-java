007c1940c0a9b95b6243b032ed3d6c3356acc3b6
Student Shruti <shrutisharma87860@gmail.com>
2026-08-08 22:40:33 +0530

    Fixed a bug in Relative Sort

--- src/Sorting/RelativeSortArray.java
+++ src/Sorting/RelativeSortArray.java
@@ -8,42 +8,43 @@ public class RelativeSortArray {
         System.out.println();
     }
 
-    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+    public static int[] relativeSort(int[] arr1, int[] arr2) {
         int length = 0;
         for(int i=0; i<arr1.length; i++) {
             length = Math.max(length, arr1[i]);
         }
 
-        int[] countfreq = new int[length+1];
+        int[] count = new int[length+1];
         for(int i=0; i<arr1.length; i++) {
-            countfreq[arr1[i]]++;
+            count[arr1[i]]++;
         }
 
-        int[] ans = new int[arr1.length];
+        int[] nums = new int[arr1.length];
         int index = 0;
         for(int i=0; i<arr2.length; i++) {
-            while(countfreq[arr2[i]] > 0) {
-                ans[index] = arr2[i];
+            while(count[arr2[i]] > 0) {
+                nums[index] = arr2[i];
                 index++;
-                countfreq[arr2[i]]--;
+                count[arr2[i]]--;
             }
         }
 
-        for(int i=0; i<countfreq.length; i++) {
-            while(countfreq[i] > 0) {
-                ans[index] = i;
+        for(int i=0; i<count.length; i++) {
+            while(count[i] > 0) {
+                nums[index] = i;
                 index++;
-                countfreq[i]--;
+                count[i]--;
             }
         }
-        return ans;
+
+        return nums;
     }
     public static void main(String[] args) {
         int[] arr1 = {2,3,1,3,2,4,6,7,9,2,19};
         int[] arr2 = {2,1,4,3,9,6};
         print(arr1);
         print(arr2);
-        int[] a = relativeSortArray(arr1, arr2);
+        int[] a = relativeSort(arr1, arr2);
         print(a);
     }
 }
