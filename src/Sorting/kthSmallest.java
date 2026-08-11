a9944a0336df9007d1f2a58f7a7efa0f8430c8b5
Student Shruti <shrutisharma87860@gmail.com>
2026-08-11 23:43:36 +0530

    Practiced for kth Smallest Element in an array

---
+++ src/Sorting/kthSmallest.java
@@ -0,0 +1,34 @@
+package src.Sorting;
+
+public class kthSmallest {
+    public static void print(int[] nums) {
+        for(int i : nums) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static int kthsmallestElement(int[] nums, int k) {
+        int n = nums.length;
+        for(int i=0; i < k; i++) {
+            int min = Integer.MAX_VALUE, minIndex = -1;
+
+            for(int j = i; j < n; j++) {
+                if(nums[j] < min) {
+                    min = nums[j];
+                    minIndex = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        return nums[k-1];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9, 2, 1, 5, 3, 8};
+        print(nums);
+        System.out.println(kthsmallestElement(nums, 3));
+    }
+}
