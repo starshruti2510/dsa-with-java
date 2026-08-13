b9bd9471bf5f594f5bfcdbea8b767295b75f05f8
Student Shruti <shrutisharma87860@gmail.com>
2026-08-13 23:36:49 +0530

    Practice Bubble Sort by Ascending

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -10,8 +10,8 @@ public class BubbleSort {
 
     public static void bubbleAscending(int[] arr) {
         int n = arr.length;
-        for(int i=0; i<=n-1; i++) {
-            for(int j=0; j<n-1; j++) {
+        for(int i = 0; i <= n-1; i++) {
+            for(int j = 0; j < n-1; j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
@@ -19,25 +19,6 @@ public class BubbleSort {
                 }
             }
         }
-        print(arr);
-
-        //---- Optimized COde-----//
-        // ----- Ascending Order ------ //
-        // int swap = 0;
-        // for(int i=0; i<=n-1; i++) {
-        //     for(int j=0; j<n-1; j++){
-        //         if(arr[j] < arr[j+1]) {
-        //             int temp = arr[j];
-        //             arr[j] = arr[j+1];
-        //             arr[j+1] = temp;
-
-        //             swap++;
-        //         }
-        //     }
-        //     if(swap == 0) {
-        //         break;
-        //     }
-        // }
     }
     public static void bubbleDescending(int[] arr) {
         int n = arr.length;
@@ -75,7 +56,6 @@ public class BubbleSort {
         print(arr);
         System.out.println("Bubble Sort Ascending");
         bubbleAscending(arr);
-        System.out.println("Bubble Sort Descending");
-        bubbleDescending(arr);
+        print(arr);
     }
 }
