6a7f07554f046ec449d6fe80aba04882368eb331
Student Shruti <shrutisharma87860@gmail.com>
2026-08-14 00:08:54 +0530

    Practiced Optimized Bubble Sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -10,16 +10,33 @@ public class BubbleSort {
 
     public static void bubbleAscending(int[] arr) {
         int n = arr.length;
+        // for(int i = 0; i <= n-1; i++) {
+        //     for(int j = 0; j < n-1; j++) {
+        //         if(arr[j] > arr[j+1]) {
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+
+        // Optimized COde
+        int swap = 0;
         for(int i = 0; i <= n-1; i++) {
             for(int j = 0; j < n-1; j++) {
                 if(arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swap++;
                 }
+            }
+            if(swap == 0) {
+                break;
             }
         }
     }
+
     public static void bubbleDescending(int[] arr) {
         int n = arr.length;
         for(int i=0; i<=n-1; i++) {
