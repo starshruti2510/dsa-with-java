24e4a7c9440192c73d88400ed4dc28d1737225a5
Student Shruti <shrutisharma87860@gmail.com>
2026-08-15 00:04:38 +0530

    Practised SelectionSort

---
+++ src/Sorting/Revision/RevisedSelectionSmallest.java
@@ -0,0 +1,32 @@
+package src.Sorting.Revision;
+
+public class RevisedSelectionSmallest {
+    public static void print(int[] nums) {
+        for(int i : nums) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void selectionSort(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++) {
+            int min = nums[i], minIndex = i;
+            for(int j = i+1; j < n; j++) {
+                if(nums[j] < min) {
+                    min = nums[j];
+                    minIndex = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {9, 2, 1, 5, 4, 7};
+        print(nums);
+        selectionSort(nums);
+        print(nums);
+    }
+}
