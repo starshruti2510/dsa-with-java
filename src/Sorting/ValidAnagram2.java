7148e910b36b3e491ba0973c4f5f6a843c948430
Student Shruti <shrutisharma87860@gmail.com>
2026-09-03 00:09:02 +0530

    Solved Valid Anagram using Arrays and Sorting

---
+++ src/Sorting/ValidAnagram2.java
@@ -0,0 +1,34 @@
+package src.Sorting;
+
+import java.util.Arrays;
+
+public class ValidAnagram2 {
+    // 1st method is Covered in Arrays Folder
+
+    // This is 2nd method ------> using Arrays and Sorting
+    public static boolean isAnagram2(String s, String t) {
+        int[] freq = new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            freq[c-'a']--;
+        }
+        for(int i=0;i<26;i++){
+         if(freq[i]!=0){
+            return false;//ellam 0 aagirkanum.
+         }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(s);
+        System.out.println(t);
+
+        System.out.println(isAnagram(s, t));
+    }
+}
